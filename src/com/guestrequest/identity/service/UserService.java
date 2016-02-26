4eb5d91fac14ba194b1e5eeeba2d631e9ca62666
ERCARVAL <ercarval@gmail.com>
2016-02-26 11:35:38 -0300

    configuracao do spring no web.xml

--- src/com/guestrequest/identity/service/UserService.java
+++ src/com/guestrequest/identity/service/UserService.java
@@ -1,12 +1,17 @@
 package com.guestrequest.identity.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.guestrequest.identity.repository.UserRepository;
 
 public class UserService {
 	
+	@Autowired
 	private UserRepository repository;
 	
 	public UserService() {
+		
+		
 	}
 
 
