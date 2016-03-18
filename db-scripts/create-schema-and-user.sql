ace7dd3a9ec618846797c0fc81d6549a16237ec8
ERCARVAL <ercarval@gmail.com>
2016-03-18 10:18:52 -0300

    add schema sql

---
+++ db-scripts/create-schema-and-user.sql
@@ -0,0 +1,6 @@
+create schema 'guest_request';
+
+CREATE USER 'guest_request'@'localhost' IDENTIFIED BY 'guest_request';
+
+GRANT ALL PRIVILEGES ON *.* TO 'guest_request'@'localhost' IDENTIFIED BY 'guest_request' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
