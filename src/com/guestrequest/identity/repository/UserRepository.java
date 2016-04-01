c91feb460102a1e5eebc6a7fdbf936f8d69abb72
ERCARVAL <ercarval@gmail.com>
2016-04-01 09:48:55 -0300

    add mysql jar and add userRepository impl

--- src/com/guestrequest/identity/repository/UserRepository.java
+++ src/com/guestrequest/identity/repository/UserRepository.java
@@ -1,25 +1,44 @@
 package com.guestrequest.identity.repository;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.guestrequest.identity.User;
 import com.guestrequest.shared.datasource.MegaDataSource;
 
 @Repository
 public class UserRepository {
-
-	@Autowired
-	private MegaDataSource datasource;
+	
+	private EntityManagerFactory emf;
 
 	public UserRepository() {
-		System.out.println("UserRepository instanciado ");
+		emf = Persistence.createEntityManagerFactory("guestRequestPU");
 	}
 	
 	@PostConstruct
 	public void init() {
-		System.out.println("....... datasource : " + datasource);
+	}
+	
+	public User create (User user) {
+		
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(user);
+		em.getTransaction().commit();
+		
+		return user;
+	}
+	
+	
+	public User findById ( Long id ) {	
+		EntityManager em = emf.createEntityManager();	
+		return em.find( User.class , id);		
 	}
 	
 	
